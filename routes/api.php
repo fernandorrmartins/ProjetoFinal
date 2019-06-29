3b66fcd673b901225c40c7e2444e3ddaa1ab65d0
Lucas Rosa Silverio <lucasrsilver@gmail.com>
2019-06-29 11:43:55 -0300

    criaçao dos controllers e das rotas

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/listarVeiculos', 'VeiculoController@getAll');
+Route::get('/veiculo/get/{id}', 'VeiculoController@show');
+Route::post('/veiculo/update/{id}', 'VeiculoController@update');
+Route::post('/veiculo/store', 'VeiculoController@store');
+
